38d30fb1fbbe99cd12c09126f2f4fdaafb263f9a
Mauro-Alexandre <mauro06aps@gmail.com>
2026-08-11 10:58:45 -0300

    feat: add Tailwind CSS integration and create reusable Botao component; update styles and configurations

---
+++ frontend/src/components/Botao.tsx
@@ -0,0 +1,16 @@
+type BotaoProps = {
+    texto: string;
+    cor: string;
+    corHover?: string;
+};
+
+function Botao({ texto, cor, corHover }: BotaoProps) 
+{
+    return (
+        <button className={`rounded-lg ${cor} text-white px-4 py-2  ${corHover}`}>
+            {texto}
+        </button>
+    )
+}
+
+export default Botao;
