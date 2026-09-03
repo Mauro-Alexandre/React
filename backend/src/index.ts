4bfd4ac361944fa7d752fbd2985b6bf259fe4d89
Mauro-Alexandre <mauro06aps@gmail.com>
2026-09-03 11:30:07 -0300

    react

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express, { type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+const contatos =
+[
+    { id: 1, nome: "Mauro", email: "mauro@teste.com"},
+    { id: 2, nome: "Samuel", email: "samuel@teste.com"},
+    { id: 3, nome: "Yan", email: "yane@teste.com"},
+]
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/contatos', (req: Request, res: Response) => {
+    res.json(contatos);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado em http://localhost:${port}`);
+});
