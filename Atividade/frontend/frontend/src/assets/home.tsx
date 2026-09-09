fe6a375fcbfcb02021ea348e523b8ab67b34564f
Mauro-Alexandre <mauro06aps@gmail.com>
2026-09-09 08:53:55 -0300

    mudança no header react

--- Atividade/frontend/frontend/src/assets/home.tsx
+++ Atividade/frontend/frontend/src/assets/home.tsx
@@ -3,61 +3,8 @@ import Header from "./header";
 function Home()
 {
     return (
-
-        <main>
-            <Header />
-<<<<<<< HEAD
-
-            <div>
-                <div className="">
-                    <h1 className="">Bem-vindo ao GameZone!</h1>
-=======
-            {/* <div className="contaniner">
-                <div className="apresentacao">
-                    <h2>Bem-vindo ao GameZone!</h2>
->>>>>>> f1cd82f2b1af071199528fdcc427115c98346917
-
-                    <p>Aqui você encontra uma seleção especial de jogos incríveis para todos os gostos.</p>
-
-                    <p>Explore nosso catálogo e descubra novas aventuras!</p>
-
-                    <button className="btn-catalogo"><a href="/jogos">Acessar catálogo</a></button>
-                </div>
-
-                <div className="">
-                    <div className="">
-                        <img src="" alt="" />
-
-                        <h2>Minecraft</h2>
-                        <h3>Aventura</h3>
-
-                        <p>Explore mundos infinitos e construa o que imaginar.</p>
-                    </div>
-                    <div className="">
-                        <img src="" alt="" />
-
-                        <h2>Stardew Valley</h2>
-                        <h3>Simulação</h3>
-
-                        <p>Viva uma vida tranquila no campo e cuide da sua fazenda.</p>
-                    </div>
-                    <div className="">
-
-                    </div>
-                    <div className="">
-                        
-                    </div>
-                </div>
-            </div>
-<<<<<<< HEAD
-
-            <Footer />
-=======
-            <Footer /> */}
->>>>>>> f1cd82f2b1af071199528fdcc427115c98346917
-        </main>
+        <Header />
     )
 }
-
 
 export default Home;
